2749b488788a13e55ca063de1712ecf8681ea343
Paul John <butadpj@gmail.com>
2021-12-14 22:31:51 +0800

    Add a state query parameter - to avoid CSRF attacks and to carry the user_email (used as main identification)

--- src/utils/twitchAuthForwardUrl.js
+++ src/utils/twitchAuthForwardUrl.js
@@ -1,16 +1,18 @@
+import { randomString } from "./randomString";
 
 
 export const twitchAuthForwardUrl = ({
   clientId,
   authRedirectUri,
   scope = [],
+  email,
 }) => {
   const base = `https://id.twitch.tv/oauth2/authorize`;
 
   if (scope.length > 0) {
     return `${base}?client_id=${clientId}&redirect_uri=${authRedirectUri}&response_type=code&scope=${scope.join(
       '%20',
-    )}&force_verify=true`;
+    )}&state=${randomString(20)}:email${email}: &force_verify=true`;
   }
 
   throw Error('Must define at least one (1) scope');
