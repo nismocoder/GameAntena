d137ba065ebe86b54d7fe7ac0766f39b92c61565
Paul John <butadpj@gmail.com>
2021-12-14 22:39:40 +0800

    Add new utility function and re-export it - randomString()

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 export { smallImage } from './smallImage';
+export { randomString } from './randomString';
 export { twitchAuthForwardUrl } from './twitchAuthForwardUrl';
 export { getLocalStorageItem } from './getLocalStorageItem';
 export { setLocalStorageItem } from './setLocalStorageItem';
