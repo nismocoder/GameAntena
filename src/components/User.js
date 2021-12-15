3d5413077af581491b461866067b2ecb37228b7f
Paul John <butadpj@gmail.com>
2021-12-15 22:33:25 +0800

    Refactor authenticateUser -> change name to handleLogin and clean up axios fetch && Add twitch_videos and unlink twitch account feature

--- src/components/User.js
+++ src/components/User.js
@@ -1,48 +1,69 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import { updateUserAuthInfo } from '../actions/authAction';
+import { loginUser, logoutUser, updateUserAuthInfo } from '../actions/authAction';
 import { setLocalStorageItem, twitchAuthForwardUrl } from '../utils';
+import { unlinkTwitchAccountURL } from '../API';
 
 const User = () => {
-  const { user, isLoggedIn } = useSelector((state) => state.auth);
+  const { user, isLoggedIn, accessToken } = useSelector((state) => state.auth);
 
   const dispatch = useDispatch();
 
-  const authenticateUser = async () => {
+  const handleLogin = async () => {
     const base = process.env.REACT_APP_BACKEND_URL;
 
-    try {
-      const result = await fetch(`${base}/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: 'admin@sample.com',
-          password: 'admin123',
-        }),
-      });
+    const loginCredentials = {
+      email: 'admin@sample.com',
+      password: 'admin123',
+    }
 
-      const { error, message, access_token: accessToken, email } = await result.json();
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }
+
+    try {
+      const result = await axios.post(
+        `${base}/login`,
+        loginCredentials,
+        config
+      );
+
+      const { error, message, access_token: accessToken, email, userId } = result.data;
 
       if (error) return alert(message);
 
+      dispatch(loginUser(email, accessToken));
+      dispatch(updateUserAuthInfo(userId, accessToken));
 
-      dispatch({ type: 'LOGIN_SUCCESS', payload: { email, accessToken } });
-      setLocalStorageItem('email', email, 30);
       setLocalStorageItem('accessToken', accessToken, 30);
-
-      dispatch(updateUserAuthInfo(email, accessToken));
-
+      setLocalStorageItem('userId', userId, 30);
     } catch (error) {
       console.log(error);
     }
   }
 
-  const logoutUser = () => {
-    dispatch({ type: "LOGOUT" });
+  const handleLogout = () => {
+    dispatch(logoutUser());
+
     localStorage.removeItem("email");
     localStorage.removeItem("accessToken");
+  }
+
+  const handleUnlink = async () => {
+    try {
+      await axios.get(unlinkTwitchAccountURL(user.id), {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    dispatch(updateUserAuthInfo(user.id, accessToken));
   }
 
   return (
@@ -57,7 +78,8 @@ const User = () => {
             {user.twitch_email && <h4>Twitch Email: {user.twitch_email}</h4>}
             {user.twitch_display_picture && <h4>Twitch Profile Image: <img width={50} src={user.twitch_display_picture} alt="twitch-avatar" /></h4>}
 
-            {!user.twitch_user_id && (
+
+            {!user.twitch_user_id ? (
               <a href={twitchAuthForwardUrl({
                 clientId: process.env.REACT_APP_TWITCH_CLIENT_ID,
                 authRedirectUri: `${process.env.REACT_APP_BACKEND_URL}/login`,
@@ -68,13 +90,51 @@ const User = () => {
                   Link your twitch acc.
                 </button>
               </a>
+            ) : (
+              <>
+                {user.twitch_videos.length > 0 ? (
+                  <div>
+                    <h4>Twitch Videos:</h4>
+                    {user.twitch_videos.map((video) => {
+                      const { twitch_id, title, thumbnail_url, url } = video;
+                      return (
+                        <a style={{
+                          boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+                          width: '20rem',
+                          textAlign: 'center',
+                          display: 'block',
+                        }} target='_blank' href={url} rel="noreferrer">
+                          <div
+                            key={twitch_id}
+                            className="twitch-video"
+                            style={{
+                              boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+                              textAlign: 'center',
+
+                            }}
+                          >
+                            <h4>Video ID: {twitch_id}</h4>
+                            <p>Title: {title}</p>
+                            <img style={{ margin: '0 auto' }} src={thumbnail_url} alt="video-thumbnail" width={200} height={200} />
+                          </div>
+                        </a>
+                      )
+                    })}
+                  </div>
+                ) : (
+                  <p>You don't have any twitch videos</p>
+                )}
+                <button onClick={handleUnlink}>
+                  Unlink your twitch acc.
+                </button>
+              </>
             )}
 
           </div>
-          <button onClick={logoutUser}>Logout</button>
+          <button onClick={handleLogout}>Logout</button>
         </>
       ) : (
-        <button onClick={authenticateUser}>Login</button>
+        <button onClick={handleLogin}>Login</button>
       )
       }
     </div >
