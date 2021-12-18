e40c671500a931790ed171bbeeeba0fa7bf38973
Paul John <butadpj@gmail.com>
2021-12-18 19:29:59 +0800

    Remove extra space in url

--- src/utils/twitchAuthForwardUrl.js
+++ src/utils/twitchAuthForwardUrl.js
@@ -12,7 +12,7 @@ export const twitchAuthForwardUrl = ({
   if (scope.length > 0) {
     return `${base}?client_id=${clientId}&redirect_uri=${authRedirectUri}&response_type=code&scope=${scope.join(
       '%20',
-    )}&state=${randomString(20)}:email${email}: &force_verify=true`;
+    )}&state=${randomString(20)}:email${email}:&force_verify=true`;
   }
 
   throw Error('Must define at least one (1) scope');
