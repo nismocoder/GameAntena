4b624371ca94d9c1ceb469da697544dd1737b0a1
Paul John <butadpj@gmail.com>
2022-01-06 01:12:34 +0800

    Add new utility function(s) - kNUmberFormatter.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,4 @@ export { getPlatformImages } from './getPlatformImages';
 export { twitchAuthForwardUrl } from './twitchAuthForwardUrl';
 export { getLocalStorageItem } from './getLocalStorageItem';
 export { setLocalStorageItem } from './setLocalStorageItem';
+export { kNUmberFormatter } from './kNUmberFormatter';
