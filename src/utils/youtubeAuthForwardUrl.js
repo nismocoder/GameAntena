65be1fb059f148425a45b3f88e8ff4a060af12eb
Paul John <butadpj@gmail.com>
2022-01-11 14:55:07 +0800

    Add new utility function - youtubeAuthForwardUrl.js

---
+++ src/utils/youtubeAuthForwardUrl.js
@@ -0,0 +1,19 @@
+import { randomString } from "./randomString";
+
+
+export const youtubeAuthForwardUrl = ({
+  clientId,
+  authRedirectUri,
+  scope = [],
+  email,
+}) => {
+  const base = `https://accounts.google.com/o/oauth2/v2/auth`;
+
+  if (scope.length > 0) {
+    return `${base}?client_id=${clientId}&redirect_uri=${authRedirectUri}&response_type=code&scope=${scope.join(
+      '%20',
+    )}&state=${randomString(20)}:email${email}`;
+  }
+
+  throw Error('Must define at least one (1) scope');
+};
