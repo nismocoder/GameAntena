88876fedffcdff0cbb56c02cd22502c142af7d5f
Paul John <butadpj@gmail.com>
2022-02-01 14:14:02 +0800

    Replace redux with -> @reduxjs/toolkit (redux-thunk included by default)

--- src/store.js
+++ src/store.js
@@ -1,20 +1,18 @@
-import { combineReducers, createStore, compose, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { authReducer, gamesReducer, detailReducer, uiReducer } from "./reducers";
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  authReducer,
+  gamesReducer,
+  detailReducer,
+  uiReducer,
+} from './reducers';
 
-const rootreducer = combineReducers({
-  auth: authReducer,
-  games: gamesReducer,
-  detail: detailReducer,
-  ui: uiReducer
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    games: gamesReducer,
+    detail: detailReducer,
+    ui: uiReducer,
+  },
 });
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootreducer,
-  composeEnhancer(applyMiddleware(thunk)),
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 export default store;
