50acc3d1f9405058bbcd236465084fc028056e9a
Paul John <butadpj@gmail.com>
2022-02-02 11:35:26 +0800

    Install new package(s) - react-query & react-query/devtools && Configure react-query

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 //Redux setup
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
 import store from './store';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
         <App />
-      </BrowserRouter>
-    </Provider>
+        <ReactQueryDevtools />
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
