5adb02f0bc2cb136ceaabe3a5a27c8d3484ed37c
Paul John <butadpj@gmail.com>
2022-02-07 19:52:32 +0800

    Fix bug in useRouteGuard's parameter - Shouldn't destructure the parameter (not a React component)

--- src/hooks/useRouteGuard.js
+++ src/hooks/useRouteGuard.js
@@ -4,14 +4,14 @@ import { useHistory } from 'react-router-dom';
 
 import { useSelector } from 'react-redux';
 
-const useRouteGuard = ({ redirectTo = '/' }) => {
+const useRouteGuard = (redirectTo = '/') => {
   const history = useHistory();
 
-  const { isLoggedIn } = useSelector(state => state.auth);
+  const { isLoggedIn } = useSelector((state) => state.auth);
 
   React.useEffect(() => {
     if (isLoggedIn) history.push(redirectTo);
   }, [history, isLoggedIn, redirectTo]);
-}
+};
 
-export default useRouteGuard
+export default useRouteGuard;
